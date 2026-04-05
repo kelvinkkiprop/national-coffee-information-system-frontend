80620c76b56c48520e482820047e96046b3c8b73
kelvinkkiprop <kelvinkiprop5@gmail.com>
2026-04-05 21:50:37 +0300

    DD

---
+++ dist/permitting-management-system-frontend/browser/chunk-33ZHMS5M.js
@@ -0,0 +1,157 @@
+import{a as K,b as U}from"./chunk-DDVCJNKJ.js";import{a as J}from"./chunk-4DMHNX4S.js";import{a as Q,b as $}from"./chunk-H5MZEWEJ.js";import{$c as V,Aa as n,Ac as E,Ba as l,Db as f,I as k,Ib as B,J as u,L as P,Mc as z,Nc as C,Oc as y,Pc as H,Q as s,R as L,Sb as N,X as j,Za as e,_b as T,ea as m,eb as d,hb as x,kc as R,kd as O,la as D,ld as h,ma as F,mc as W,md as b,na as S,nd as q,qa as p,xc as g,ya as a,za as t,zb as G,zc as v}from"./chunk-SFMD2PHH.js";var I=class o{constructor(i,r){this.mAppContextService=i;this.http=r}allItems(){let i=`${Q.base_url}/dashboard`;return this.http.get(i,this.mAppContextService.getHttpOptions())}static{this.\u0275fac=function(r){return new(r||o)(P($),P(B))}}static{this.\u0275prov=k({token:o,factory:o.\u0275fac,providedIn:"root"})}};var _=()=>({gx:3,gy:3});function Z(o,i){o&1&&l(0,"app-progress")}function ee(o,i){o&1&&(e(0,`
+            `),s(),l(1,"svg",27),e(2,`
+          `))}function te(o,i){o&1&&(e(0,`
+            `),s(),l(1,"svg",28),e(2,`
+          `))}function ne(o,i){o&1&&(e(0,`
+            `),s(),l(1,"svg",29),e(2,`
+          `))}function ie(o,i){o&1&&(e(0,`
+            `),s(),l(1,"svg",30),e(2,`
+          `))}var w=class o{constructor(i,r){this.mDownloadService=i;this.mToastrService=r;this.mProgress=j(!1)}ngOnInit(){this.index()}index(){this.mProgress.set(!0),this.mDownloadService.allItems().subscribe({next:i=>{i&&(this.mConstructionPermits=i.data.construction_permits,this.mAdvertisingApplications=i.data.advertising_applications,this.mConstructionSiteBoards=i.data.construction_site_boards,this.mComplianceAndEnforcements=i.data.compliance_and_enforcements,this.myConstructionPermits=i.data.my_construction_permits,this.myComplianceAndEnforcements=i.data.my_compliance_and_enforcements),this.mProgress.set(!1)},error:i=>{i.error.message&&this.mToastrService.error(i.error.message),this.mProgress.set(!1)}})}static{this.\u0275fac=function(r){return new(r||o)(D(I),D(J))}}static{this.\u0275cmp=F({type:o,selectors:[["app-index"]],standalone:!1,decls:219,vars:27,consts:[[4,"ngIf"],[1,"mb-3"],[3,"gutter"],["xs","12"],["color","danger",1,"d-flex","align-items-center","justify-content-between"],["routerLink","/downloads",1,"btn","btn-success","btn-sm","ms-3"],[1,"mt-2","text-muted"],["xs","6","md","4"],["routerLink","/construction-permits",1,"text-decoration-none"],[1,"h-100","shadow-sm","hover-shadow"],[1,"card-title"],[1,"card-text","text-medium-emphasis"],["routerLink","/parcel-allocation-worksheet",1,"text-decoration-none"],["routerLink","/downloads",1,"text-decoration-none"],["routerLink","/advertising-application",1,"text-decoration-none"],["routerLink","/construction-site-board",1,"text-decoration-none"],["routerLink","/compliance-and-enforcement",1,"text-decoration-none"],["md","3","xl","3"],["color","info","inverse1","",1,"mb-4",3,"title","value"],["cTemplateId","widgetIconTemplate"],["color","warning","inverse1","",1,"mb-4",3,"title","value"],["color","success","inverse1","",1,"mb-4",3,"title","value"],["color","danger","inverse1","",1,"mb-4",3,"title","value"],["xs","12","lg","6"],[1,"mb-4",3,"title","value"],["cIcon","","name","cilPuzzle","size","xl"],["cIcon","","name","cilList","size","xl"],["cIcon","","height","36","name","cilPuzzle"],["cIcon","","height","36","name","cilDescription"],["cIcon","","height","36","name","cilCursor"],["cIcon","","height","36","name","cilList"]],template:function(r,c){r&1&&(p(0,Z,1,0,"app-progress",0),t(1,"div"),e(2,`
+
+  `),t(3,"div",1),e(4,`
+    `),t(5,"c-row",2),e(6,`
+      `),t(7,"c-col",3),e(8,`
+        `),t(9,"c-callout",4),e(10,`
+          `),t(11,"span"),e(12,`
+            Looking for guidelines or templates? Explore our resources to get started quickly and efficiently.
+          `),n(),e(13,`
+          `),t(14,"a",5),e(15,`
+            Learn More
+          `),n(),e(16,`
+        `),n(),e(17,`
+      `),n(),e(18,`
+    `),n(),e(19,`
+  `),n(),e(20,`
+
+  `),t(21,"div",1),e(22,`
+    `),t(23,"c-row",2),e(24,`
+      `),t(25,"c-col",3),e(26,`
+        `),t(27,"h5",6),e(28,"Quick Links"),n(),e(29,`
+      `),n(),e(30,`
+      `),t(31,"c-col",7),e(32,`
+        `),t(33,"a",8),e(34,`
+          `),t(35,"c-card",9),e(36,`
+            `),t(37,"c-card-body"),e(38,`
+              `),t(39,"h6",10),e(40,"Construction Permit"),n(),e(41,`
+              `),t(42,"p",11),e(43,`
+                Apply for approval to begin construction after meeting all requirements.
+              `),n(),e(44,`
+            `),n(),e(45,`
+          `),n(),e(46,`
+        `),n(),e(47,`
+      `),n(),e(48,`
+      `),t(49,"c-col",7),e(50,`
+        `),t(51,"a",12),e(52,`
+          `),t(53,"c-card",9),e(54,`
+            `),t(55,"c-card-body"),e(56,`
+              `),t(57,"h6",10),e(58,"Parcel Application Worksheet (PAW)"),n(),e(59,`
+              `),t(60,"p",11),e(61,`
+                Request changes to land use, subdivision, or property extensions.
+              `),n(),e(62,`
+            `),n(),e(63,`
+          `),n(),e(64,`
+        `),n(),e(65,`
+      `),n(),e(66,`
+      `),t(67,"c-col",7),e(68,`
+        `),t(69,"a",13),e(70,`
+          `),t(71,"c-card",9),e(72,`
+            `),t(73,"c-card-body"),e(74,`
+              `),t(75,"h6",10),e(76,"Guidelines, Codes & Forms"),n(),e(77,`
+              `),t(78,"p",11),e(79,`
+                Access planning guidelines, building codes, and application forms.
+              `),n(),e(80,`
+            `),n(),e(81,`
+          `),n(),e(82,`
+        `),n(),e(83,`
+      `),n(),e(84,`
+      `),t(85,"c-col",7),e(86,`
+        `),t(87,"a",14),e(88,`
+          `),t(89,"c-card",9),e(90,`
+            `),t(91,"c-card-body"),e(92,`
+              `),t(93,"h6",10),e(94,"Advertising Application"),n(),e(95,`
+              `),t(96,"p",11),e(97,`
+                Apply for billboard and signage approvals.
+              `),n(),e(98,`
+            `),n(),e(99,`
+          `),n(),e(100,`
+        `),n(),e(101,`
+      `),n(),e(102,`
+      `),t(103,"c-col",7),e(104,`
+        `),t(105,"a",15),e(106,`
+          `),t(107,"c-card",9),e(108,`
+            `),t(109,"c-card-body"),e(110,`
+              `),t(111,"h6",10),e(112,"Construction Site Board"),n(),e(113,`
+              `),t(114,"p",11),e(115,`
+                Apply and manage construction site board permits.
+              `),n(),e(116,`
+            `),n(),e(117,`
+          `),n(),e(118,`
+        `),n(),e(119,`
+      `),n(),e(120,`
+      `),t(121,"c-col",7),e(122,`
+        `),t(123,"a",16),e(124,`
+          `),t(125,"c-card",9),e(126,`
+            `),t(127,"c-card-body"),e(128,`
+              `),t(129,"h6",10),e(130,"Compliance and Enforcement"),n(),e(131,`
+              `),t(132,"p",11),e(133,`
+                Monitor compliance, conduct inspections, and enforce regulations on approved developments.
+              `),n(),e(134,`
+            `),n(),e(135,`
+          `),n(),e(136,`
+        `),n(),e(137,`
+      `),n(),e(138,`
+    `),n(),e(139,`
+  `),n(),e(140,`
+
+  `),t(141,"div",1),e(142,`
+    `),t(143,"c-row",2),e(144,`
+      `),t(145,"c-col",3),e(146,`
+        `),t(147,"h5",6),e(148,"System Insights"),n(),e(149,`
+      `),n(),e(150,`
+    `),n(),e(151,`
+
+    `),t(152,"c-row"),e(153,`
+      `),t(154,"c-col",3),e(155,`
+        `),t(156,"h6",6),e(157,"Internal"),n(),e(158,`
+      `),n(),e(159,`
+      `),t(160,"c-col",17),e(161,`
+        `),t(162,"c-widget-stat-c",18),e(163,`
+          `),p(164,ee,3,0,"ng-template",19),e(165,`
+        `),n(),e(166,`
+      `),n(),e(167,`
+      `),t(168,"c-col",17),e(169,`
+        `),t(170,"c-widget-stat-c",20),e(171,`
+          `),p(172,te,3,0,"ng-template",19),e(173,`
+        `),n(),e(174,`
+      `),n(),e(175,`
+      `),t(176,"c-col",17),e(177,`
+        `),t(178,"c-widget-stat-c",21),e(179,`
+          `),p(180,ne,3,0,"ng-template",19),e(181,`
+        `),n(),e(182,`
+      `),n(),e(183,`
+      `),t(184,"c-col",17),e(185,`
+        `),t(186,"c-widget-stat-c",22),e(187,`
+          `),p(188,ie,3,0,"ng-template",19),e(189,`
+        `),n(),e(190,`
+      `),n(),e(191,`
+    `),n(),e(192,`
+
+    `),t(193,"c-row",2),e(194,`
+      `),t(195,"c-col",3),e(196,`
+        `),t(197,"h6",6),e(198,"External"),n(),e(199,`
+      `),n(),e(200,`
+      `),t(201,"c-col",23),e(202,`
+        `),t(203,"c-widget-stat-e",24),e(204,`
+          `),s(),l(205,"svg",25),e(206,`
+        `),n(),e(207,`
+      `),n(),e(208,`
+      `),L(),t(209,"c-col",23),e(210,`
+        `),t(211,"c-widget-stat-e",24),e(212,`
+          `),s(),l(213,"svg",26),e(214,`
+        `),n(),e(215,`
+      `),n(),e(216,`
+    `),n(),e(217,`
+
+  `),n(),e(218,`
+
+`),n()),r&2&&(a("ngIf",c.mProgress()),m(5),a("gutter",x(23,_)),m(18),a("gutter",x(24,_)),m(120),a("gutter",x(25,_)),m(19),a("value",d(c.mConstructionPermits||0))("title","Construction Permits"),m(8),a("value",d(c.mConstructionSiteBoards||0))("title","Construction Site Boards"),m(8),a("value",d(c.mAdvertisingApplications||0))("title","Advertising Applications"),m(8),a("value",d(c.mComplianceAndEnforcements||0))("title","Compliance & Enforcements"),m(7),a("gutter",x(26,_)),m(10),a("value",d(c.myConstructionPermits))("title","Construction Permits"),m(8),a("value",d(c.myComplianceAndEnforcements))("title","Compliance & Enforcements"))},dependencies:[G,N,K,C,H,y,v,E,g,h,W,R,b],encapsulation:2})}};var oe=[{path:"",redirectTo:"index",pathMatch:"full"},{path:"index",component:w,data:{title:"Home"}}],A=class o{static{this.\u0275fac=function(r){return new(r||o)}}static{this.\u0275mod=S({type:o})}static{this.\u0275inj=u({imports:[f,T.forChild(oe),T]})}};var X=class o{static{this.\u0275fac=function(r){return new(r||o)}}static{this.\u0275mod=S({type:o})}static{this.\u0275inj=u({imports:[f,A,U,C,z,y,v,E,g,V,O,h,q,b]})}};export{X as DashboardModule};
---
+++ dist/permitting-management-system-frontend/browser/chunk-J2TWABF7.js
@@ -0,0 +1,94 @@
+import{a as ee,b as te}from"./chunk-DDVCJNKJ.js";import{a as Z}from"./chunk-4DMHNX4S.js";import{a as h,b as Y}from"./chunk-H5MZEWEJ.js";import{Aa as n,Ac as q,Ba as x,Bc as z,Cc as J,Db as w,I as N,Ib as P,J as f,Ka as E,L as F,Ma as b,Nc as K,O as v,Oa as p,Oc as Q,P as u,Q as _,Qc as W,R as S,Rb as H,Va as O,X as j,Za as e,_a as I,_b as V,ea as d,jd as X,kc as A,la as g,lc as B,ma as R,na as C,nc as L,pb as y,qa as a,ya as c,yb as $,za as t,zb as G,zc as U}from"./chunk-SFMD2PHH.js";var D=class i{constructor(m,o){this.mAppContextService=m;this.http=o}allItems(){let m=`${h.base_url}/downloads`;return this.http.get(m,this.mAppContextService.getHttpOptions())}static{this.\u0275fac=function(o){return new(o||i)(F(Y),F(P))}}static{this.\u0275prov=N({token:i,factory:i.\u0275fac,providedIn:"root"})}};function oe(i,m){i&1&&x(0,"app-progress")}function me(i,m){if(i&1){let o=E();t(0,"div",9),e(1,`
+                `),t(2,"div",10),e(3,`
+                  `),t(4,"div",11),e(5,`
+                    `),t(6,"div",12),_(),x(7,"svg",13),n(),e(8,`
+                    `),S(),t(9,"div",14),e(10),n(),e(11,`
+                    `),t(12,"div"),e(13,`
+                      `),t(14,"button",15),b("click",function(){let r=v(o).$implicit,s=p(2);return u(s.onDownload(r.type_id,r.filename))}),e(15,"View"),n(),e(16,`
+                    `),n(),e(17,`
+                  `),n(),e(18,`
+                `),n(),e(19,`
+              `),n()}if(i&2){let o=m.$implicit;d(10),I(o.name)}}function re(i,m){if(i&1&&(t(0,"div"),e(1,`
+            `),t(2,"div",7),e(3,`
+              `),a(4,me,20,1,"div",8),e(5,`
+            `),n(),e(6,`
+          `),n()),i&2){let o=p();d(4),c("ngForOf",o.mDevelopmentCodes)}}function ae(i,m){i&1&&(e(0,`
+              `),t(1,"p",16),e(2,"No items"),n(),e(3,`
+          `))}function le(i,m){if(i&1){let o=E();t(0,"div",9),e(1,`
+                `),t(2,"div",10),e(3,`
+                  `),t(4,"div",11),e(5,`
+                    `),t(6,"div",12),_(),x(7,"svg",17),n(),e(8,`
+                    `),S(),t(9,"div",14),e(10),n(),e(11,`
+                    `),t(12,"div"),e(13,`
+                      `),t(14,"button",18),b("click",function(){let r=v(o).$implicit,s=p(2);return u(s.onDownload(r.type_id,r.filename))}),e(15,"Show"),n(),e(16,`
+                    `),n(),e(17,`
+                  `),n(),e(18,`
+                `),n(),e(19,`
+              `),n()}if(i&2){let o=m.$implicit;d(10),I(o.name)}}function de(i,m){if(i&1&&(t(0,"div"),e(1,`
+            `),t(2,"div",7),e(3,`
+              `),a(4,le,20,1,"div",8),e(5,`
+            `),n(),e(6,`
+          `),n()),i&2){let o=p();d(4),c("ngForOf",o.mDevelopmentGuidelines)}}function ce(i,m){i&1&&(e(0,`
+              `),t(1,"p",16),e(2,"No items"),n(),e(3,`
+          `))}function pe(i,m){if(i&1){let o=E();t(0,"div",9),e(1,`
+                `),t(2,"div",10),e(3,`
+                  `),t(4,"div",11),e(5,`
+                    `),t(6,"div",12),_(),x(7,"svg",19),n(),e(8,`
+                    `),S(),t(9,"div",14),e(10),n(),e(11,`
+                    `),t(12,"div"),e(13,`
+                      `),t(14,"button",20),b("click",function(){let r=v(o).$implicit,s=p(2);return u(s.onDownload(r.type_id,r.filename))}),e(15,"Download"),n(),e(16,`
+                    `),n(),e(17,`
+                  `),n(),e(18,`
+                `),n(),e(19,`
+              `),n()}if(i&2){let o=m.$implicit;d(10),I(o.name)}}function se(i,m){if(i&1&&(t(0,"div"),e(1,`
+            `),t(2,"div",7),e(3,`
+              `),a(4,pe,20,1,"div",8),e(5,`
+            `),n(),e(6,`
+          `),n()),i&2){let o=p();d(4),c("ngForOf",o.mForms)}}function xe(i,m){i&1&&(e(0,`
+              `),t(1,"p",16),e(2,"No items"),n(),e(3,`
+          `))}var T=class i{constructor(m,o,l){this.mDownloadService=m;this.mToastrService=o;this.router=l;this.mDevelopmentCodes={};this.mDevelopmentGuidelines={};this.mForms={};this.mProgress=j(!1)}ngOnInit(){this.index()}index(){this.mProgress.set(!0),this.mDownloadService.allItems().subscribe({next:m=>{m&&(this.mDevelopmentCodes=m.data.development_codes,this.mDevelopmentGuidelines=m.data.development_guidelines,this.mForms=m.data.forms),this.mProgress.set(!1)},error:m=>{m.error.message&&this.mToastrService.error(m.error.message),this.mProgress.set(!1)}})}onDownload(m,o){let l=`${h.base_url}/downloads/${m}/${o}`;window.open(l,"_blank")}static{this.\u0275fac=function(o){return new(o||i)(g(D),g(Z),g(H))}}static{this.\u0275cmp=R({type:i,selectors:[["app-index"]],standalone:!1,decls:75,vars:7,consts:[["noItemsTemplate",""],[4,"ngIf"],["xs","12"],[1,"mb-4"],[1,"card-title","text-muted"],[1,"mb-3"],[4,"ngIf","ngIfElse"],[1,"row"],["class","col-md-3 mb-3",4,"ngFor","ngForOf"],[1,"col-md-3","mb-3"],[1,"card","shadow","h-100"],[1,"card-body","text-center"],[1,"text-danger"],["cIcon","","name","cilFolder",1,"btn-icon","text-success"],[1,"text-bold"],["cButton","",1,"btn","btn-danger","btn-sm",3,"click"],[1,"p-5","text-center","text-muted"],["cIcon","","name","cilFolderOpen",1,"btn-icon","text-danger"],["cButton","",1,"btn","btn-success","btn-sm",3,"click"],["cIcon","","name","cilFile",1,"btn-icon","text-warning"],["cButton","",1,"btn","btn-link","btn-sm",3,"click"]],template:function(o,l){if(o&1&&(a(0,oe,1,0,"app-progress",1),t(1,"c-row"),e(2,`
+
+  `),t(3,"c-col",2),e(4,`
+    `),t(5,"c-card",3),e(6,`
+      `),t(7,"c-card-header"),e(8,`
+        `),t(9,"div",4)(10,"strong"),e(11,"Development Codes"),n()(),e(12,`
+      `),n(),e(13,`
+      `),t(14,"c-card-body"),e(15,`
+        `),t(16,"div",5),e(17,`
+          `),a(18,re,7,1,"div",6),e(19,`
+          `),a(20,ae,4,0,"ng-template",null,0,y),e(22,`
+        `),n(),e(23,`
+      `),n(),e(24,`
+    `),n(),e(25,`
+  `),n(),e(26,`
+
+  `),t(27,"c-col",2),e(28,`
+    `),t(29,"c-card",3),e(30,`
+      `),t(31,"c-card-header"),e(32,`
+        `),t(33,"div",4)(34,"strong"),e(35,"Development Guidelines"),n()(),e(36,`
+      `),n(),e(37,`
+      `),t(38,"c-card-body"),e(39,`
+        `),t(40,"div",5),e(41,`
+          `),a(42,de,7,1,"div",6),e(43,`
+          `),a(44,ce,4,0,"ng-template",null,0,y),e(46,`
+        `),n(),e(47,`
+      `),n(),e(48,`
+    `),n(),e(49,`
+  `),n(),e(50,`
+
+  `),t(51,"c-col",2),e(52,`
+    `),t(53,"c-card",3),e(54,`
+      `),t(55,"c-card-header"),e(56,`
+        `),t(57,"div",4)(58,"strong"),e(59,"Forms"),n()(),e(60,`
+      `),n(),e(61,`
+      `),t(62,"c-card-body"),e(63,`
+        `),t(64,"div",5),e(65,`
+          `),a(66,se,7,1,"div",6),e(67,`
+          `),a(68,xe,4,0,"ng-template",null,0,y),e(70,`
+        `),n(),e(71,`
+      `),n(),e(72,`
+    `),n(),e(73,`
+  `),n(),e(74,`
+
+
+`),n()),o&2){let r=O(21);c("ngIf",l.mProgress()),d(18),c("ngIf",l.mDevelopmentCodes.length>0)("ngIfElse",r),d(24),c("ngIf",l.mDevelopmentGuidelines.length>0)("ngIfElse",r),d(24),c("ngIf",l.mForms.length>0)("ngIfElse",r)}},dependencies:[$,G,ee,U,q,z,K,Q,A],encapsulation:2})}};var fe=[{path:"",redirectTo:"index",pathMatch:"full"},{path:"index",component:T,data:{title:"Downloads"}}],M=class i{static{this.\u0275fac=function(o){return new(o||i)}}static{this.\u0275mod=C({type:i})}static{this.\u0275inj=f({imports:[w,V.forChild(fe),V]})}};var ne=class i{static{this.\u0275fac=function(o){return new(o||i)}}static{this.\u0275mod=C({type:i})}static{this.\u0275inj=f({imports:[w,M,te,J,W,L,X,B]})}};export{ne as DownloadModule};
